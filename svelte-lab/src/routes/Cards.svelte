76907d7dd09ff516584e613415f2d29c6cf6bfd4
Ensar <ensar_mehmedovic@hotmail.com>
2026-02-26 22:29:37 +0100

    commit

--- svelte-lab/src/routes/Cards.svelte
+++ svelte-lab/src/routes/Cards.svelte
@@ -7,7 +7,7 @@
 <div class="cards-container">
     {#each data as item}
     <div class="card">
-        <i>{item.icon}</i>
+       <span class={item.icon}></span>
         <h2>{item.title}</h2>
         <p>{item.description}</p>
     </div>
@@ -18,19 +18,35 @@
 
 <style lang="scss">
 @import '../app.scss';
+.cards-container{
+    display: grid;
+    justify-content: center;
+    gap: 20px;
+    width: 100%;
+    grid-template-columns: repeat(3,300px);
+    
+}
 .card{
-    height:270px;
-    width: 270px;
-    border:2px solid red;
-    padding:5px;
+    margin: 10px;
+    height:300px;
+    width: 300px;
+    border:none;
+    background-color: $primaryLightBlack;
+    padding:25px;
     h2{
         color:$primaryWhite;
         font-size: 30px;
     }
     p{
         font-size: 14px;
-        
+        margin-top: 20px;
         color: $primaryGray;
+    }
+    span{
+        font-size: 30px;
+         color: $primaryBlue;
+           margin-bottom: 10px;
+
     }
 }
   
