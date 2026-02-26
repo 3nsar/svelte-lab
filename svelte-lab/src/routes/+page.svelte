9bb5946080bca85b58999c7ea8913e04da1266e8
Ensar <ensar_mehmedovic@hotmail.com>
2026-02-26 22:04:41 +0100

    commit

--- svelte-lab/src/routes/+page.svelte
+++ svelte-lab/src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
     import Hero from "./Hero.svelte";
+    import Cards from "./Cards.svelte";
    import '../app.scss';
 </script>
 
 <Hero/>
+<Cards/>
+
 
 <style lang="scss">
   $primaryBlack: #0f0f0f;
---
+++ svelte-lab/src/routes/Cards.svelte
@@ -0,0 +1,37 @@
+<script>
+    import data from '../data.json'
+      import 'font-awesome/css/font-awesome.min.css';
+       
+</script>
+
+<div class="cards-container">
+    {#each data as item}
+    <div class="card">
+        <i>{item.icon}</i>
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+    </div>
+  {/each}
+</div>
+
+
+
+<style lang="scss">
+@import '../app.scss';
+.card{
+    height:270px;
+    width: 270px;
+    border:2px solid red;
+    padding:5px;
+    h2{
+        color:$primaryWhite;
+        font-size: 30px;
+    }
+    p{
+        font-size: 14px;
+        
+        color: $primaryGray;
+    }
+}
+  
+</style>
