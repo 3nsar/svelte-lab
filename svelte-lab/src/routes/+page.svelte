7db89992af15af5b1629b8ed153bdef77c4d6c30
Ensar <ensar_mehmedovic@hotmail.com>
2026-02-26 20:58:44 +0100

    commit

---
+++ svelte-lab/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Hero from "./Hero.svelte";
+   import '../app.scss';
+</script>
+
+<Hero/>
+
+<style lang="scss">
+  $primaryBlack: #0f0f0f;
+
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
+    background-color: $primaryBlack;
+  }
+</style>
---
+++ svelte-lab/src/routes/Hero.svelte
@@ -0,0 +1,17 @@
+<div class="container">
+
+    <div class="title">
+        <h1>CREATIVE <span class="rainbow">STUDIO</span></h1>
+        <h2>Excellence projecting is devonshire dispatched remarkably on estimating. Side in so life past. Continue indulged speaking the was out horrible for domestic.</h2>
+    </div>
+
+
+
+</div>
+
+
+
+
+<style lang="scss">
+  
+</style>
